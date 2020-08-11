5c008e987e0bd9543f6b5ad105cd4dca81511453
Sui Gn <natsynapses@gmail.com>
2020-08-10 20:55:58 -0500

    1
    1

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const routes = require("./routes");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,8 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/* GET home page. */
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Caelestibus' });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
