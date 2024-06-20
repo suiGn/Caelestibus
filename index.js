65d2637e7085a6867e17905eaae5aa71bcc383ef
Sui Gn <natsynapses@gmail.com>
2024-06-19 22:29:48 -0500

    -
    -

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("Caelestibus/index.js loaded.")
+// argument || angle
+let r = 0;
+
+/*
+W=re 
+iθ
+where:
+𝑟 is the magnitude (or modulus) of the complex number.
+𝜃 is the argument (or angle) of the complex number.
+𝑒 is the base of the natural logarithm.
+𝑖 is the imaginary unit, defined by i2 = −1
+*/
---
+++ orchestra/strings.js
@@ -0,0 +1,6 @@
+//e Vns I div. && Violas div.
+
+//f Vns II div && 'Cellos div
+//g Violas div. && D. basses div. 
+//etc.
+
